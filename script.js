a6d6ffac3f6cd7e340154f96e6efedacb0630748
SoorajV1995 <34004926+SoorajV1995@users.noreply.github.com>
2018-02-03 22:38:13 +0530

    DigitalClock

--- script.js
+++ script.js
@@ -4,15 +4,46 @@ let minutes;
 let seconds;
 let time;
 let clock;
+let amPm;
 
-function digitalClock() {
+window.onload=function digitalClock() {
 	timeStamp = new Date();
 	
 	hours = timeStamp.getHours();
 	minutes = timeStamp.getMinutes();
 	seconds = timeStamp.getSeconds();
 
-	time = `${hours} : ${minutes} : ${seconds}`
-	document.write(time);
+	hours = checkTime(hours);
+	minutes = checkTime(minutes);
+	seconds = checkTime(seconds);
+
+	amPm = amOrpm(hours);
+
+	hours = hourFormat(hours);
+
+	time = `${hours} : ${minutes} : ${seconds} ${amPm}`
+	document.getElementById("clock").innerHTML=time;
+	setTimeout(digitalClock,1000);
 }
-digitalClock();
+
+function checkTime(i) {
+	if(i < 10) {
+		i = "0"+i;
+	}
+	return i;
+}
+
+function amOrpm(hour) {
+	if(hour<12){
+		return 'AM';
+	} else if(hour>=12){
+		return 'PM';
+	}
+}
+
+function hourFormat(hour) {
+	if(hour > 12) {
+		hour = hour-12;
+	}
+	return hour;
+}
